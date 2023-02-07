14a5e76deab2dd7b759491b2b00ad6d1d9e6261a
Jchen869 <jchen15@ggc.edu>
2023-02-07 11:37:56 -0500

    For Chap 3

---
+++ src/AlphabeticalWords.java
@@ -0,0 +1,6 @@
+public class AlphabeticalWords {
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ src/TriangleSize.java
@@ -0,0 +1,5 @@
+public class TriangleSize {
+    public static void main(String[] args){
+
+    }
+}
